2607a006957a8da6b1271e9e6e16dc535087cffb
tydolla00 <90355178+tydolla00@users.noreply.github.com>
2024-10-18 12:13:46 -0400

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "@/components/modetoggle";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ModeToggle />
+    </main>
+  );
+}
