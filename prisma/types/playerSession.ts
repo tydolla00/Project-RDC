7fa67c45930e2222d1ff406c737fb320dee847eb
Scott Hargrove <scott7c1@gmail.com>
2024-11-02 21:33:56 -0400

    Implement Prisma + Model Structure (#5)
    db model creation
    ---------
    
    Co-authored-by: shargrove09 <scott.7c1@gmail.com>
    Co-authored-by: tydolla00 <90355178+tydolla00@users.noreply.github.com>

---
+++ prisma/types/playerSession.ts
@@ -0,0 +1,24 @@
+import { Prisma } from "@prisma/client";
+
+// Things we need for each Mario Kart Session
+// RDC names, stat names and values
+const playerSessionWithPlayerStats =
+  Prisma.validator<Prisma.PlayerSessionDefaultArgs>()({
+    include: {
+      set: true,
+      player: {
+        select: {
+          playerName: true,
+        },
+      },
+      playerStats: {
+        include: {
+          gameStat: true,
+        },
+      },
+    },
+  });
+
+export type PlayerSessionWithStats = Prisma.PlayerSessionGetPayload<
+  typeof playerSessionWithPlayerStats
+>;
---
+++ src/lib/providers.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import config from "./config";
+
+if (typeof window !== "undefined") {
+  posthog.init(config.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: config.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+  });
+}
+
+export function CSPostHogProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element {
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+}
