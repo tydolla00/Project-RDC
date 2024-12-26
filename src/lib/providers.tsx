48a2eeda57513bb8bc4275cb1c3ed46955eab9c9
tydolla00 <90355178+tydolla00@users.noreply.github.com>
2024-12-25 23:41:15 -0500

    fix build errors

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -6,21 +6,20 @@ import config from "./config";
 
 import { useEffect, type JSX } from "react";
 
-useEffect(() => {
-  posthog.init(config.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: config.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
-
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === "development") posthog.debug();
-    },
-  });
-}, []);
-
 export function CSPostHogProvider({
   children,
 }: {
   children: React.ReactNode;
 }): JSX.Element {
+  useEffect(() => {
+    posthog.init(config.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host: config.NEXT_PUBLIC_POSTHOG_HOST,
+      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === "development") posthog.debug();
+      },
+    });
+  }, []);
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
