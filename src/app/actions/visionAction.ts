80e16d3872282c7acc1fa233b99e869c176e48be
Shargrove09 <8881163+Shargrove09@users.noreply.github.com>
2025-01-14 12:59:00 -0500

    I can see! I can FIGHT!

--- src/app/actions/visionAction.ts
+++ src/app/actions/visionAction.ts
@@ -1,6 +1,8 @@
-import { AzureKeyCredential } from "@azure/core-auth";
-import DocumentIntelligence from "@azure-rest/ai-document-intelligence";
-import sanitizedConfig from "@/lib/config";
+import DocumentIntelligence, {
+  getLongRunningPoller,
+  AnalyzeResultOperationOutput,
+  isUnexpected,
+} from "@azure-rest/ai-document-intelligence";
 
 const client = DocumentIntelligence(
   process.env["NEXT_PUBLIC_DOCUMENT_INTELLIGENCE_ENDPOINT"]!,
@@ -8,14 +10,36 @@ const client = DocumentIntelligence(
     key: process.env["NEXT_PUBLIC_DOCUMENT_INTELLIGENCE_API_KEY"]!,
   },
 );
-const modelId = "custom-rdc";
+const modelId = "RDC-Custom-Model";
+
+interface visionResults {
+  winner?: string;
+  blueTeam: Array<visionPlayer>;
+  orangeTeam: Array<visionPlayer>;
+}
+
+interface visionPlayer {
+  name: string;
+  stats: rlVisionStats;
+}
+
+interface rlVisionStats {
+  score: string | undefined;
+  goals: string | undefined;
+  assists: string | undefined;
+  saves: string | undefined;
+  shots: string | undefined;
+}
 
 export const analyzeScreenShotTest = async () => {
+  // TODO: Add Loading State
+  // TODO: Fix env config
+  // TODO: Convert to base64
   console.log(
     "Document Endpointz: ",
     process.env["NEXT_PUBLIC_DOCUMENT_INTELLIGENCE_ENDPOINT"],
   );
-  console.log("Document API Key: ", sanitizedConfig.YOUTUBE_API_KEY);
+
   try {
     if (!process.env["NEXT_PUBLIC_DOCUMENT_INTELLIGENCE_ENDPOINT"]) {
       throw new Error("Missing DOCUMENT_INTELLIGENCE_ENDPOINT in .env");
@@ -25,21 +49,78 @@ export const analyzeScreenShotTest = async () => {
       );
     }
 
-    const response = await fetch(
-      `${process.env["NEXT_PUBLIC_DOCUMENT_INTELLIGENCE_ENDPOINT"]}documentintelligence/documentModels/{modelId}:analyze?_overload=analyzeDocument&api-version=2024-11-30`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Ocp-Apim-Subscription-Key":
-            process.env["NEXT_PUBLIC_DOCUMENT_INTELLIGENCE_API_KEY"]!,
-        },
-        body: JSON.stringify({
+    const response = await client
+      .path("/documentModels/{modelId}:analyze", modelId)
+      .post({
+        contentType: "application/json",
+        body: {
           urlSource: "https://i.imgur.com/QTvH8QA.png",
-        }),
-      },
-    );
-    console.log("Response: ", response);
+        },
+        queryParameters: {
+          locale: "en-US",
+        },
+      });
+
+    if (isUnexpected(response)) {
+      throw response.body.error;
+    }
+
+    const poller = await getLongRunningPoller(client, response);
+    const result = (await poller.pollUntilDone())
+      .body as AnalyzeResultOperationOutput;
+    ("0");
+    if (!result.analyzeResult || !result.analyzeResult.documents) {
+      throw new Error("Analyze result or documents are undefined");
+    }
+    const teams = result.analyzeResult.documents[0].fields;
+
+    // Should check to ensure that there are two teams
+    console.log("Teams: ", teams);
+
+    // Should check that number of players is equal for both teams
+    if (!teams) {
+      throw new Error("Teams data is undefined");
+    }
+
+    const visionResult: visionResults = {} as visionResults;
+    // team: {type 'array', valueArray: players: { name, stats...} , confidence: number}
+    Object.entries(teams).forEach(([team, players]) => {
+      console.log("Team Name: ", team);
+      console.log("Players: ", players);
+
+      // valueInteger === confirmed number and is integer properly -- content holds the actual value detected by the model and is always present
+      if (team == "BluePlayers" && players.valueArray) {
+        visionResult.blueTeam = players.valueArray.map((player) => {
+          return {
+            name: player.valueObject?.PlayerName?.content || "Unknown",
+            stats: {
+              score: player.valueObject?.Score?.content || undefined,
+              goals: player.valueObject?.Goals?.content || undefined,
+              assists: player.valueObject?.Assists?.content || undefined,
+              saves: player.valueObject?.Saves?.content || undefined,
+              shots: player.valueObject?.Shots?.content || undefined,
+            },
+          };
+        });
+      }
+
+      if (team == "OrangePlayers" && players.valueArray) {
+        visionResult.orangeTeam = players.valueArray.map((player) => {
+          return {
+            name: player.valueObject?.PlayerName?.content || "Unknown",
+            stats: {
+              score: player.valueObject?.Score?.content || undefined,
+              goals: player.valueObject?.Goals?.content || undefined,
+              assists: player.valueObject?.Assists?.content || undefined,
+              saves: player.valueObject?.Saves?.content || undefined,
+              shots: player.valueObject?.Shots?.content || undefined,
+            },
+          };
+        });
+      }
+    });
+
+    console.log("Vision Result: ", visionResult);
   } catch (error) {
     console.error(error);
   }
