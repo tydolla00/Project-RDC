66bfba32df3d325739e6012207982c44d63a9d24
tydolla00 <ktyty9@aol.com>
2025-08-03 20:56:21 -0400

    fix build errors

--- src/hooks/useViewTransition.ts
+++ src/hooks/useViewTransition.ts
@@ -1,11 +1,12 @@
-// "use client";
+"use client";
 
-// const useViewTransition = () => {
-//   const startTransition = (fn: () => unknown) => {
-//     if (!document.startViewTransition) fn;
-//     else document.startViewTransition(() => fn);
-//   };
+const useViewTransition = () => {
+  const startTransition = (fn: void) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    if (!document.startViewTransition) fn;
+    else document.startViewTransition(() => fn);
+  };
 
-//   return { startTransition };
-// };
-// export default useViewTransition;
+  return { startTransition };
+};
+export default useViewTransition;
--- src/lib/game-processors/game-processor-utils.ts
+++ src/lib/game-processors/game-processor-utils.ts
@@ -67,7 +67,8 @@ export type GameProcessor = {
     requiresCheck: boolean,
   ) => {
     status: VisionResultCodes;
-    data: unknown;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    data: any;
     message: string;
   };
 };
