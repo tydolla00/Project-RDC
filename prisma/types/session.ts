915c11a2b89e93e6dd17c86cca10284fd55d4a34
tydolla00 <ktyty9@aol.com>
2025-08-10 20:53:23 -0400

    update schema to reflect mvp

--- prisma/types/session.ts
+++ prisma/types/session.ts
@@ -4,6 +4,8 @@ import { Prisma } from "@prisma/client";
 const enrichedSession = Prisma.validator<Prisma.SessionDefaultArgs>()({
   include: {
     Game: true,
+    mvp: true,
+    dayWinners: true,
     sets: {
       include: {
         setWinners: true,
@@ -24,3 +26,14 @@ const enrichedSession = Prisma.validator<Prisma.SessionDefaultArgs>()({
 });
 
 export type EnrichedSession = Prisma.SessionGetPayload<typeof enrichedSession>;
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace PrismaJson {
+    type MvpOutput = {
+      statName: string;
+      sum: number;
+      average?: number | undefined;
+    }[];
+  }
+}
